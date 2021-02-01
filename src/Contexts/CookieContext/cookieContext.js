8c078c7946c68b5bd7f22154d63914a244412e92
aaliakinci <aliakiinci@gmail.com>
2021-02-01 14:50:56 +0300

    userDetail Page created - cookie errors fixed , header improvement for cookie

--- src/Contexts/CookieContext/cookieContext.js
+++ src/Contexts/CookieContext/cookieContext.js
@@ -1,7 +1,9 @@
 import { createContext } from 'react';
 import cookie from 'js-cookie';
+import jwt_decode from "jwt-decode";
 
 const CookieContext = createContext({
+	userFromCookie:()=>{},
   isCookie: ()=>{},
   setUserCookie: () => {},
 });
@@ -17,10 +19,18 @@ export const CookieProvider = ({ children }) => {
 		if (!mycookie) return 0;
 		return 1;
 	};
+	const userFromCookie = (name) => {
+		const bearerToken = cookie.getJSON(name)
+		const token = bearerToken.replace('Bearer ', '')
+		const user = jwt_decode(token)
+		return user
+	}
 	const values = {
 		setUserCookie,
 		isCookie,
+		userFromCookie
 	};
+	
 
 	return <CookieContext.Provider value={values}>{children}</CookieContext.Provider>;
 };
