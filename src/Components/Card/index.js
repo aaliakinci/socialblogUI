ad7dd24a8440f91707ec893f243b61d69a75913c
Onur Karal <santobixua@gmail.com>
2021-02-11 00:09:32 +0300

    recent and main posts design

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,10 +1,11 @@
 import {useEffect,useContext,useState} from 'react'
 import LikeContext from '../../Contexts/LikeContext/likeContext';
 import './blogCard.css'
+import moment from "moment";
 import blogPost01 from '../../assets/images/blog-post-01.jpg';
 import {Link} from 'react-router-dom'
 
- function BlogCard({ title, description, content, user,createAt, id }) {
+ function BlogCard({ title, description, content, user,createAt, id,hashtags }) {
 	const {getLikesByArticleId} = useContext(LikeContext)
 	const [likes, setLikes] = useState()
 	console.log(id);
@@ -28,16 +29,24 @@ import {Link} from 'react-router-dom'
                 <img src={blogPost01} alt="" />
             </div>
             <div className="down-content">
-                <span>Lifestyle(Hashtag)</span>
-                <Link to={`/article/${id}`}>
-                    <h4>{title}</h4>
-                </Link>
+			<Link to={`/article/${id}`}>
+            <span>{title}</span>
+                </Link> 
+                <h6>#{hashtags}!Hastags</h6>
+
                 <ul className="post-info">
 									{
 										user && <Link to={`/articles/${user[0].username}`}>{user[0].username}</Link>
 									}
                     {/* <li><a href="javascript"> {user_id} </a></li> */}
-                    <li><a href="javascript"> {createAt} </a></li>
+					
+                    <li>
+					<li></li>
+                    <Link to={`/article/${id}`}>
+                    <a href="javascript">  {moment({createAt}).format("l")} </a>
+                    </Link>
+
+                    </li>
                     <li><a href="javascript">12 Comments</a></li>
                 </ul>
                 <p>{description}</p>
