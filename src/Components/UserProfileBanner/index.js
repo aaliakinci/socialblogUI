e1163aced1e18156d3d064710f377ac61ac29fa0
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-02-11 17:08:52 +0300

    profile user-info changed.

--- src/Components/UserProfileBanner/index.js
+++ src/Components/UserProfileBanner/index.js
@@ -1,36 +1,43 @@
 import React from 'react';
 import './userProfile.css'
+
+import img1 from '../../assets/images/comment-author-01.jpg';
 function UserProfileBanner({ user }) {
 	console.log(user);
 	return (
 		<>
 			{
 				user &&
-				<div className="user-info-card">
-					<div className="user-info">
-						<div className="user-image profileUserpic">
-							<img
-								src={user.profilPicture}
-								className="img-responsive"
-								alt=""
-							/>
-						</div>
-						<div className="user-info-text">
-							<div className="d-flex">
-								<div>
-									<span className="user-name">{user.name} {user.surname}</span>
-								</div>
-								<div className="follow-btn">
-									<button>Follow</button>
-								</div>
+				<div className="user-info-card mx-0 card">
+					<div className="card-body">
+						<div className="user-info">
+							<div className="user-image profileUserpic">
+								<img
+									src={user.profilPicture}
+									className="img-responsive"
+									alt=""
+								/>
+								<img src={img1} alt="" className="img-responsive" />
 							</div>
-							<div className="create-at">
-								{user.createAt}
+							<div className="user-info-text">
+								<div className="d-flex">
+									<div>
+										<span className="user-name">{user.name} {user.surname}</span>
+									</div>
+									<div className="follow-btn">
+										<button>Follow</button>
+									</div>
+									<div className="message-btn">
+										<button>Message</button>
+									</div>
+								</div>
+								<div className="create-at">
+									{user.createAt}
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-
 			}
 		</>
 	);
