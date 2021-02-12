6756c92add11a687faa2007b28701b10a8dfd351
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-02-13 01:16:40 +0300

    likes and followers added.

--- src/Components/UserProfileBanner/index.js
+++ src/Components/UserProfileBanner/index.js
@@ -7,34 +7,36 @@ function UserProfileBanner({ user }) {
 		<>
 			{
 				user &&
-				<div className="user-info-card mx-0 card">
-					<div className="card-body">
-						<div className="user-info">
-							<div className="user-image profileUserpic">
-								<img
-									src={user.profilPicture}
-									className="img-responsive"
-									alt=""
-								/>
+				<div className="user-info-card mx-0">
+					<div className="user-info">
+						<div className="user-image profileUserpic">
+							<img
+								src={user.profilPicture}
+								className="img-responsive"
+								alt=""
+							/>
 
+						</div>
+						<div className="user-info-text">
+							<div className="d-flex">
+								<div>
+									<span className="user-name">{user.name} {user.surname}</span>
+								</div>
+								<div className="follow-btn">
+									<button>Follow</button>
+								</div>
+								<div className="message-btn">
+									<button>Message</button>
+								</div>
 							</div>
-							<div className="user-info-text">
-								<div className="d-flex">
-									<div>
-										<span className="user-name">{user.name} {user.surname}</span>
-									</div>
-									<div className="follow-btn">
-										<button>Follow</button>
-									</div>
-									<div className="message-btn">
-										<button>Message</button>
-									</div>
-								</div>
-								<div className="create-at">
-									{user.createAt}
-								</div>
+							<div className="create-at">
+								{user.createAt}
 							</div>
 						</div>
+					</div>
+					<div className="d-flex align-items-end">
+						<i class="fa fa-users fa-2x friends"></i>
+						<i class="fa fa-heart fa-2x likes"></i>
 					</div>
 				</div>
 			}
