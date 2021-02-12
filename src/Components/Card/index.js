91c28531d39c9ad56103d6708d95191bc6ddf016
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-02-12 13:18:24 +0300

    card component edited.

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -5,7 +5,7 @@ import moment from "moment";
 import blogPost01 from '../../assets/images/blog-post-01.jpg';
 import { Link } from 'react-router-dom'
 
-function BlogCard({ title, description, content, user, createAt, id }) {
+function BlogCard({ title, description, content, user, createAt, id, hashtags }) {
 	const { getLikesByArticleId } = useContext(LikeContext)
 	const [likes, setLikes] = useState()
 	console.log(id);
@@ -22,29 +22,31 @@ function BlogCard({ title, description, content, user, createAt, id }) {
 			area.innerHTML = content
 		}
 	}, [content])
+
 	return (
 		<div className="blog-post">
 			<div className="blog-thumb">
 				<img src={blogPost01} alt="" />
 			</div>
 			<div className="down-content">
-				<span>Lifestyle(Hashtag)</span>
 				<Link to={`/article/${id}`}>
-					<h4>{title}</h4>
+					<h4><span>{title}</span></h4>
 				</Link>
+				<h6>#{hashtags}!Hastags</h6>
 				<ul className="post-info">
 					{
 						user && <Link to={`/articles/${user[0].username}`} className="link">{user[0].username}</Link>
 					}
-					<li><a href="javascript">{createAt} </a></li>
-					<li><a href="javascript">12 Comments</a></li>
+					<li><Link to="javascript">{createAt} </Link></li>
+					<li><Link to="javascript">12 Comments</Link></li>
 				</ul>
 				<p>{description}</p>
-				{content && <div className="card-body" id="contentArea"></div>
+				{
+					content && <div className="card-body" id="contentArea"></div>
 				}
 
-			</div>
-		</div>
+			</div >
+		</div >
 	)
 }
 export default BlogCard
