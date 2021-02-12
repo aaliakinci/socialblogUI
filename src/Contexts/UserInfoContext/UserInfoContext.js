253c0052f5a0b4eaaacf2bd2bbb73d26a62b7c8e
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-02-13 00:31:31 +0300

    user info added to the profile component.

--- src/Contexts/UserInfoContext/UserInfoContext.js
+++ src/Contexts/UserInfoContext/UserInfoContext.js
@@ -1,39 +1,46 @@
-import { useState, createContext, useEffect } from 'react'
-import BlogContext from '../BlogContext/BlogContext';
-const UserInfoContext = createContext({});
+// import axios from 'axios';
+// import { useState, createContext, useEffect } from 'react'
+// import BlogContext from '../BlogContext/BlogContext';
+// const UserInfoContext = createContext({});
 
-export const UserInforProvider = ({ children }) => {
-    const [name, setName] = useState("");
-    const [surname, setSurname] = useState("");
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [gender, setGenter] = useState(false);
+// export const UserInforProvider = ({ children }) => {
+//     const [name, setName] = useState("");
+//     const [surname, setSurname] = useState("");
+//     const [email, setEmail] = useState("");
+//     const [username, setUsername] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [phoneNumber, setPhoneNumber] = useState("");
+//     const [gender, setGenter] = useState(false);
 
+//     useEffect(() => {
+//         const getUserInfoByUsername = async () => {
+//             const url = `${process.env.REACT_APP_DEPLOY_URL}/users/${username}
+//         }`
+//             const data = await axios(url);
+//         }
+//     }, []);
 
+//     const values = {
+//         name,
+//         setName,
+//         surname,
+//         setSurname,
+//         email,
+//         setEmail,
+//         username,
+//         setUsername,
+//         password,
+//         setPassword,
+//         phoneNumber,
+//         setPhoneNumber,
+//         gender,
+//         setGenter
+//     }
 
-    const values = {
-        name,
-        setName,
-        surname,
-        setSurname,
-        email,
-        setEmail,
-        username,
-        setUsername,
-        password,
-        setPassword,
-        phoneNumber,
-        setPhoneNumber,
-        gender,
-        setGenter
-    }
+//     return <BlogContext.Provider value={values}>{children}</BlogContext.Provider>;
+// }
 
-    return <BlogContext.Provider value={values}>{children}</BlogContext.Provider>;
-}
-
-export default UserInfoContext;
+// export default UserInfoContext;
 
 
 
