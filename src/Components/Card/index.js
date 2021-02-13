3b4154892a4acea6627d20571d9d922a4816e1ba
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-02-13 05:35:38 +0300

    colors are defined as variables and integrated into css files.

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -13,24 +13,29 @@ function BlogCard({ title, description, content, user, createAt, id, hashtags })
 			area.innerHTML = content
 		}
 	}, [content])
-	
+	console.log(user)
 	return (
 		<div className="blog-post">
 			<div className="blog-thumb">
 				<img src={blogPost01} alt="" />
 			</div>
+			<div>
+				<img src={user[0].profilPicture} className="img-responsive user-img" alt="" />
+				<div className="d-inline user-info">{user[0].name}  </div>
+				<div className="d-inline user-info">{user[0].surname}</div>
+			</div>
 			<div className="down-content">
 				<Link to={`/article/${id}`}>
 					<h4><span>{title}</span></h4>
 				</Link>
-				<h6>#{hashtags}!Hastags</h6>
+				<h6>#{hashtags}</h6>
 				<ul className="post-info">
 					{
 						user && <Link to={`/articles/${user[0].username}`} className="link">{user[0].username}</Link>
 					}
-					<li>
-						<Moment format="YYYY/MM/DD"> 
-						{createAt}
+					<li className="link">
+						<Moment format="YYYY/MM/DD">
+							{createAt}
 						</Moment></li>
 					<li><Link to="javascript">12 Comments</Link></li>
 				</ul>
