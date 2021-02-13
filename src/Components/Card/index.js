e286d485f9f0bf59589bc77c48d05a6e12c760f3
aaliakinci <aliakiinci@gmail.com>
2021-02-13 15:14:36 +0300

    bug fixeds

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import './blogCard.css'
 import Moment from 'react-moment';
-import blogPost01 from '../../assets/images/blog-post-01.jpg';
 import { Link } from 'react-router-dom'
 
 function BlogCard({ title, description, content, user, createAt, id, hashtags, image }) {
@@ -34,7 +33,7 @@ function BlogCard({ title, description, content, user, createAt, id, hashtags, i
 				<Link to={`/article/${id}`}>
 					<h4><span>{title}</span></h4>
 				</Link>
-				{hashtags && hashtags.map(hashtag=>(<h6>#{hashtag.body}</h6>))}
+				{hashtags && hashtags.map(hashtag=>(<h6 key={hashtag._id}>#{hashtag.body}</h6>))}
 				<ul className="post-info">
 					<li className="link">
 						<Moment format="YYYY/MM/DD">
