d1868a4d01c867a55bc2cd074afeacf9cd46b694
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-02-13 05:37:16 +0300

    header and footer edited.

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -8,15 +8,14 @@ export default function Footer() {
                 <div className="row">
                     <div className="col-lg-12">
                         <ul className="social-icons">
-                            <li><a href="javascript">Github</a></li>
-                            <li><a href="javascript">Twitter</a></li>
-                            <li><a href="javascript">LinkedIn</a></li>
+                            <li><a href="https://github.com/Kodluyoruz">Github</a></li>
+                            <li><a href="https://www.youtube.com/channel/UCSnIf-v95OSGIv9H7d2R2lw">Youtube</a></li>
                         </ul>
                     </div>
                     <div className="col-lg-12">
                         <div className="copyright-text">
-                            <p>Copyright 2021 | Design:
-                                <a rel="nofollow" href="https://templatemo.com" target="_parent">TemplateMo</a>
+                            <p>Kodluyoruz 2021 |
+                                  <a href="https://www.kodluyoruz.org/" className="ml-1">Noas</a>
                             </p>
                         </div>
                     </div>
